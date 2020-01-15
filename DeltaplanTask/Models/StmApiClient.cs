dfaf721e6c5757dbc012162bd8f69a827664ea1e
Roman Bushuev <Eqip3u.dem@yandex.ru>
2020-01-15 10:40:23 +0500

    scanffold

---
+++ DeltaplanTask/Models/StmApiClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeltaplanTask.Models
+{
+    public partial class StmApiClient
+    {
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string ApiKey { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+    }
+}
